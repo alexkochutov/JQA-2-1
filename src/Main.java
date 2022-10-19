6b1b466368bd61329ac24cba7f6d3366330c02db
Alex Kochutov <alex.kochutov@gmail.com>
2022-10-19 19:29:42 +0400

    feat: add Main class file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 1519;
+        int mileCost = 20;
+        int bonus = ticketCost / mileCost;
+        System.out.println("За билет стоимостью " + ticketCost + " начислено " + bonus + " миль");
+    }
+}
